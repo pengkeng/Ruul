c4b290627a1963b69896d2f17852808a4fce92cf
Tokhta Horqin <tokhta@outlook.com>
2017-12-18 14:54:40 +0800

    upgrade its configure

--- R/Ruul.R
+++ R/Ruul.R
@@ -7,15 +7,7 @@ Ruul <- function(x, stdopt = 0, simopt = 1) {
         stop("ERROR! stdopt must be 0, 1, ..., 3.")
     }
     if (simopt > 16 || simopt < 1) {
-        stop("ERROR! simopt must be 1, 2, ..., 13.")
-    }
-
-    if (version$os == "linux-gnu") {
-        dyn.load(paste(Sys.getenv("R_LIBS_USER"), "/", "Ruul/libs/Ruul.so", sep = ""))
-    } else if (version$os == "mingw32") {
-        dyn.load(paste(Sys.getenv("R_LIBS_USER"), "\\", "Ruul\\libs\\Ruul.dll", sep = ""))
-    } else {
-        stop("ERROR! OS must be linux-gnu or mingw32.")
+        stop("ERROR! simopt must be 1, 2, ..., 16.")
     }
 
     xstd <- Ruul.stdm(x,    opt = stdopt)
