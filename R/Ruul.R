16fca2317a852a1272b42248543b0e95e5f20a26
Tokhta Horqin <tokhta@outlook.com>
2018-02-03 10:50:56 +0800

    upgrade the struct of R code

--- R/Ruul.R
+++ R/Ruul.R
@@ -1,28 +1,55 @@
-Ruul <- function(x, scale = 0, dist = 1) {
+Ruul.scale <- function (x, scale = 1) {
     if (! requireNamespace("Rcpp", quietly = TRUE)) {
         stop("Please install Rcpp: install.packages('Rcpp')")
     }
-
     if (! is.matrix(x)) {
         stop("Please input x as matrix.")
     }
     if (! is.numeric(scale)) {
         stop("Please input scale as numeric.")
     }
-    if (! is.numeric(dist)) {
-        stop("Please input dist as numeric.")
-    }
-
     if (scale > 3 || scale < 0) {
         stop("ERROR! scale must be 0, 1, ..., 3.")
+    }
+
+    .Call("RScale", x, scale, PACKAGE = "Ruul")
+}
+
+Ruul.dist <- function (xstd, dist = 1) {
+    if (! requireNamespace("Rcpp", quietly = TRUE)) {
+        stop("Please install Rcpp: install.packages('Rcpp')")
+    }
+    if (! is.matrix(xstd)) {
+        stop("Please input x as matrix.")
+    }
+    if (! is.numeric(dist)) {
+        stop("Please input dist as numeric.")
     }
     if (dist > 16 || dist < 1) {
         stop("ERROR! dist must be 1, 2, ..., 16.")
     }
 
-    xstd <- Ruul.scale(x, scale)
+    .Call("RDist", xstd, dist, PACKAGE = "Ruul")
+}
 
-    r <- Ruul.dist(xstd, dist)
+Ruul.bind <- function (r1, r2) {
+    if (! requireNamespace("Rcpp", quietly = TRUE)) {
+        stop("Please install Rcpp: install.packages('Rcpp')")
+    }
+    if (! is.matrix(r1)) {
+        stop("Please input r1 as matrix.")
+    }
+    if (! is.matrix(r2)) {
+        stop("Please input r2 as matrix.")
+    }
+
+    .Call("RBind", r1, r2, PACKAGE = "Ruul")
+}
+
+Ruul.closure <- function(r) {
+    if (! is.matrix(r)) {
+        stop("Please input r as matrix.")
+    }
 
     rt <- Ruul.bind(r, r)
     while (! all(abs(r - rt) < 1E-4)) {  
@@ -30,5 +57,6 @@ Ruul <- function(x, scale = 0, dist = 1) {
         rt <- Ruul.bind(r, r)
     }
 
-    as.dist(r)
+    return(rt)
 }
+
